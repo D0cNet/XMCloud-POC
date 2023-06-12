1d40754656233f93a14f9ffd11c6e89fa1f3fb86
Karega Anglin <karega.anglin@perficient.com>
2023-06-12 11:32:19 -0400

    adding plugins

---
+++ src/sxastarter/src/temp/middleware-plugins.ts
@@ -0,0 +1,2 @@
+export { personalizePlugin } from 'src/lib/middleware/plugins/personalize';
+export { redirectsPlugin } from 'src/lib/middleware/plugins/redirects';
---
+++ src/sxastarter/src/temp/page-props-factory-plugins.ts
@@ -0,0 +1,5 @@
+export { componentPropsPlugin } from 'src/lib/page-props-factory/plugins/component-props';
+export { errorPagesPlugin } from 'src/lib/page-props-factory/plugins/error-pages';
+export { normalModePlugin } from 'src/lib/page-props-factory/plugins/normal-mode';
+export { personalizePlugin } from 'src/lib/page-props-factory/plugins/personalize';
+export { previewModePlugin } from 'src/lib/page-props-factory/plugins/preview-mode';
